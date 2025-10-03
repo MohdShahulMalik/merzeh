6908005a6d3c195d742b72be9203d2c473024d57
MohdShahulMalik <mohdshahulmalik@gmail.com>
2025-10-03 19:27:17 +0000

    made custom validation method for form data

--- src/auth/custom_auth.rs
+++ src/auth/custom_auth.rs
@@ -1,12 +1,15 @@
 use crate::models::registration::FormData;
+use garde::Validate;
 use anyhow::Result;
 use argon2::{password_hash::{PasswordHasher, SaltString}, Argon2, PasswordHash};
-use garde::Validate;
 use rand::rngs::OsRng;
 use crate::errors::password_hash::PasswordHashError;
+use crate::database::connection::get_db;
 
-pub fn register(form: FormData) -> Result<()>{ 
+pub async fn register(form: FormData) -> Result<()>{ 
+    let db = get_db();
     form.validate()?;
+    form.validate_uniqueness(&db).await?;
     let password_bytes = form.password.as_bytes();
     let salt = SaltString::generate(&mut OsRng);
     let argon2 = Argon2::default();
--- src/models/registration.rs
+++ src/models/registration.rs
@@ -1,5 +1,9 @@
+use anyhow::{anyhow, Context, Result};
 use garde::Validate;
 use serde::{Deserialize, Serialize};
+use surrealdb::engine::remote::ws::Client;
+use surrealdb::Surreal;
+use tokio::runtime::Runtime;
 
 #[derive(Debug, Validate, Deserialize, Serialize)]
 pub struct FormData {
@@ -19,4 +23,30 @@ pub enum UserIdentifier {
         #[garde(pattern(r"^[6-9][0-9]{9}$"))]
         String
     ),
+}
+
+impl FormData {
+    pub async fn validate_uniqueness(&self, db: &&'static Surreal<Client>) -> Result<()> {
+        let (field, value) = match &self.identifier {
+            UserIdentifier::Email(email) => ("email", email.to_string()),
+            UserIdentifier::Mobile(mobile) => ("mobile", mobile.to_string()),
+        };
+
+        let query_str = format!("SELECT * FROM user WHERE {} = $value", field);
+        let mut result = db
+            .query(&query_str)
+            .bind(("value", value))
+            .await
+            .map_err(|e| anyhow!("Database error: {}", e))?;
+
+        let res: Vec<serde_json::Value> = result
+            .take(0)
+            .map_err(|_| anyhow!("Failed to parse query result"))?;
+
+        if !res.is_empty() {
+            Err(anyhow!("{} already exists", field))
+        } else {
+            Ok(())
+        }
+    }
 }
